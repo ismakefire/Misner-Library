d96d35cca7a24c5f5f806c0aa0e570c09e62eb61
Kyle Misner <ismakefire@gmail.com>
2021-05-03 13:34:59 -0700

    Adding new graphics based utility classes.

---
+++ Scripts/Graphics/ColorUtil.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Misner.Lib.Graphical
+{
+    public static class ColorUtil
+    {
+        public static Color FromHex(uint hexCode)
+        {
+            float r = ((hexCode & 0xff0000) >> 16) / 255f;
+            float g = ((hexCode & 0xff00) >> 8) / 255f;
+            float b = (hexCode & 0xff) / 255f;
+
+            Color result = new Color(r, g, b);
+
+            return result;
+        }
+    }
+}
---
+++ Scripts/Graphics/TextureUtil.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.IO;
+
+namespace Misner.Lib.Graphical
+{
+    public static class TextureUtil
+    {
+        public static void WriteRenderTextureToImageFile(RenderTexture textureSource, string pngOutPath)
+        {
+            Texture2D encodableTexture = ConvertRenderTextureToTexture2D(textureSource);
+
+            File.WriteAllBytes(pngOutPath, encodableTexture.EncodeToPNG());
+        }
+
+        public static Texture2D ConvertRenderTextureToTexture2D(RenderTexture textureSource)
+        {
+            Texture2D result;
+
+            RenderTexture oldRT = RenderTexture.active;
+            RenderTexture.active = textureSource;
+            {
+                result = new Texture2D(textureSource.width, textureSource.height);
+                result.ReadPixels(new Rect(0, 0, textureSource.width, textureSource.height), 0, 0);
+                result.Apply();
+            }
+            RenderTexture.active = oldRT;
+
+            return result;
+        }
+    }
+}
