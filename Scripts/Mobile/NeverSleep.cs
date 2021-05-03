4925767a67e8537df056b4ace0fdc7018bf2e2b4
Kyle Misner <ismakefire@gmail.com>
2021-05-03 13:52:41 -0700

    Moving our NeverSleep behavior into our Library.

---
+++ Scripts/Mobile/NeverSleep.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Misner.Lib.Mobile
+{
+    public class NeverSleep : MonoBehaviour
+    {
+        #region MonoBehaviour
+        void Awake()
+        {
+            Screen.sleepTimeout = (int)SleepTimeout.NeverSleep;
+        }
+        #endregion
+    }
+}
