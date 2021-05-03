12dd7e99553b773451cab12fe35c442b1e302188
Kyle Misner <ismakefire@gmail.com>
2021-05-03 14:17:42 -0700

    Icon generation scripts.

---
+++ Scripts/Icon Generation/Editor/IconPrinterEditor.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEditor;
+using Misner.Lib.Graphical;
+
+namespace Misner.Lib.Icon
+{
+    /// <summary>
+    /// Icon printer editor.
+    /// 
+    /// Builds the editor UI for printing icons.
+    /// </summary>
+    [CustomEditor(typeof(IconPrinter))]
+    public class IconPrinterEditor : Editor
+    {
+        /// <summary>
+        /// Gets my printer.
+        /// 
+        /// Our reference to the scene object.
+        /// </summary>
+        /// <value>My printer.</value>
+        public IconPrinter MyPrinter
+        {
+            get
+            {
+                return (IconPrinter)target;
+            }
+        }
+
+        /// <summary>
+        /// OnInspector GUI
+        /// 
+        /// Builds our "Print" button.
+        /// </summary>
+        public override void OnInspectorGUI()
+        {
+            base.DrawDefaultInspector();
+
+            GUILayout.BeginHorizontal();
+
+            if (GUILayout.Button("Print"))
+            {
+                foreach (int iconWidth in MyPrinter._iconWidths)
+                {
+                    string filePath = string.Format("{0}/Output/icon({1}x{2}).png", Application.dataPath, iconWidth, iconWidth);
+
+                    RenderTexture iconRenderTexture = new RenderTexture(iconWidth, iconWidth, 0);
+                    Graphics.Blit(MyPrinter._renderTexture, iconRenderTexture, Vector2.one, Vector2.zero);
+
+                    TextureUtil.WriteRenderTextureToImageFile(iconRenderTexture, filePath);
+                }
+
+                AssetDatabase.Refresh();
+
+                Debug.Log("Print to Icons");
+            }
+
+            GUILayout.EndHorizontal();
+        }
+    }
+}
---
+++ Scripts/Icon Generation/IconPrinter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Misner.Lib.Icon
+{
+    /// <summary>
+    /// Icon printer
+    /// 
+    /// Scene object for printing out an array of icons for use by mobile platforms.
+    /// </summary>
+    public class IconPrinter : MonoBehaviour
+    {
+        #region SerializeField
+        [SerializeField]
+        public RenderTexture _renderTexture;
+
+        [SerializeField]
+        public int[] _iconWidths = { 36, 48, 72, 96, 144, 192 };
+        #endregion
+    }
+}
